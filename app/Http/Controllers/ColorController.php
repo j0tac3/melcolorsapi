8f41bcd9c857975ce13f443c69c5fb2616e03d0d
j0tac3 <j0s3.90jr@gmail.com>
2022-04-15 01:23:03 +0200

    Creada la ruta para los colores de una compañia

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -26,7 +26,7 @@ class ColorController extends Controller
                                 ->get();
         /* $color = Color::where('company_id', '=', $companies->id)
                     ->get(); */
-        return $companies;
+        return $companies->desc;
     }
 
     public function store(Request $request) {
