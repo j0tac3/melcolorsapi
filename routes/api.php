a1a7d968b036792db8540f6a067a75f3534f4218
j0tac3 <j0s3.90jr@gmail.com>
2022-04-14 23:35:14 +0200

    Creada la ruta para los colores de una compañia

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('colors/{company}', [UserController::class, 'getColorCompany']);
+Route::get('colors/{company}', [ColorController::class, 'getColorCompany']);
 
 Route::apiResources([
     'user' => UserController::class,
