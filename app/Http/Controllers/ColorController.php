894ab34d3417fdd5faf49f2076c1c06292574314
j0tac3 <j0s3.90jr@gmail.com>
2022-04-16 03:28:17 +0200

    Creada la ruta para los colores de una compañia

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -26,7 +26,7 @@ class ColorController extends Controller
                                 ->select('id')
                                 ->where('short_desc', $company)
                                 ->get();
-        if ($companySelected->empty()){
+        if (!$companySelected->empty()){
             $colors = [];
         } else {
             $colors = DB::table('colors')
